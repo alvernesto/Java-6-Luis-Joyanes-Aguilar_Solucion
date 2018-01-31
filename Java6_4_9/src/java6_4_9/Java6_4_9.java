abab31cdde53a3634472ec61beb620179ea07807
alvernesto <31789549+alvernesto@users.noreply.github.com>
2018-01-31 19:54:39 -0300

    Add files via upload

---
+++ Java6_4_9/src/java6_4_9/Java6_4_9.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java6_4_9;
+import java.util.Scanner;
+/**
+ *
+ * @author Álvaro Ernesto
+ */
+public class Java6_4_9 {
+static Scanner input = new Scanner (System.in);
+    
+    public static void main(String[] args) {
+        int a = input.nextInt();
+        if a<=100 System.out.println(" < 100");
+                
+    }
+    
+}
