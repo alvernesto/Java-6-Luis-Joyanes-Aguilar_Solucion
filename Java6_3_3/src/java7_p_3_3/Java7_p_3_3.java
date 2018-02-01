e704be5014e1caf373e4339a1109a9ce6ef0a469
alvernesto <31789549+alvernesto@users.noreply.github.com>
2018-02-01 00:28:44 -0300

    Add files via upload

---
+++ Java6_3_3/src/java7_p_3_3/Java7_p_3_3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java7_p_3_3;
+
+/** pag 81
+ *
+ * @author Álvaro Ernesto
+ */
+public class Java7_p_3_3 {
+
+    static String nombre = "Alvaro Ernesto"; 
+    static String direccion = "psj. Por algún lugar 557 - Chillán";
+    
+    public static void main(String[] args) {
+        System.out.println("Nombre    : " +  nombre);
+        System.out.println("Direccion : " +  direccion);
+    }
+    
+}
